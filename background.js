f3c0bc4262c8fa26b7dc32bfd474ffb252662cd8
ShengBi <shengbi@fhi-berlin.mpg.de>
2017-12-15 13:45:12 +0100

    反爬youtube
    添加头文件

--- background.js
+++ background.js
@@ -39,10 +39,16 @@ xmlHttp.onerror = () => {
 }
 
 browser.browserAction.onClicked.addListener(() => {
-  // xmlHttp.open("GET", "https://www.youtube.com/results?search_query=阅后即瞎", true)
-  xmlHttp.open("GET", "https://www.google.com/search?newwindow=1&safe=active&dcr=0&biw=1304&bih=696&ei=K-owWpSzFuy-jwTQqbfAAg&q=embed+jquery+in+firefox+extension&oq=embed+jquery+in+firefox+extension&gs_l=psy-ab.3...20577.23443.0.25372.10.10.0.0.0.0.447.1685.3-4j1.5.0....0...1c.1.64.psy-ab..5.0.0....0.uw-3fa8pVW0", true)
+  xmlHttp.open("GET", "https://www.youtube.com/results?search_query=阅后即瞎", true)
+  //xmlHttp.open("GET", "https://www.google.com/search?newwindow=1&safe=active&dcr=0&biw=1304&bih=696&ei=K-owWpSzFuy-jwTQqbfAAg&q=embed+jquery+in+firefox+extension&oq=embed+jquery+in+firefox+extension&gs_l=psy-ab.3...20577.23443.0.25372.10.10.0.0.0.0.447.1685.3-4j1.5.0....0...1c.1.64.psy-ab..5.0.0....0.uw-3fa8pVW0", true);
   // xmlHttp.open("GET", "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=2&tn=baiduhome_pg&wd=%E9%98%85%E5%90%8E%E5%8D%B3%E7%9E%8E&rsv_spt=1&oq=%25E9%2598%2585%25E5%2590%258E%25E5%258D%25B3%25E7%259E%258E&rsv_pq=c48ed0d400000944&rsv_t=7e59CNkqYBPgB973WCHCiRE%2B%2BgyHHJFCaFsv7kb2fsPY4xsE8bujUuhE7MgIF0RWdK6u&rqlang=cn&rsv_enter=0&rsv_sug=1", true)
   xmlHttp.responseType = "text"
+  xmlHttp.setRequestHeader ('Accept', 'text/html');
+  xmlHttp.setRequestHeader ('Content-Type', 'application/x-www-form-urlencoded');
+  xmlHttp.setRequestHeader ('Content-Length', '19' );
+  xmlHttp.setRequestHeader ('Accept-Charset', 'utf-8' );
+  xmlHttp.setRequestHeader ('User-Agent', 'Mozilla/5.0 (Windows NT 5.1; rv:37.0) Gecko/20100101 Firefox/37.0');
+
   xmlHttp.send()
   popUpNotification("request has been sent...waiting for respond")
   // var creating = browser.tabs.create({
