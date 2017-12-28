f17e59d63171725678e6e4b231bc1990c9bdaf44
ShengBi <shengbi@fhi-berlin.mpg.de>
2017-12-28 11:53:48 +0100

    comment insert video directly

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -57,76 +57,79 @@ function convertSearchToFeed(ObjM, VedioInfo) {
 
 
 // 读取已获得的列表
-console.log("load video list");
-//let  getListVedio = browser.storage.local.get("list_vedio");
-//getListVedio.then(onGot);
-let gettingItem = browser.storage.local.get("list_vedio");
-gettingItem.then((Obj) => {
-	if (isObjEmpty(Obj)) {
-		//console.log("list is empty");
-
-		return [];
-	} else {
-		//console.log("list is not empty");
-		//console.log(Obj);
-		//console.log(Obj.list_vedio.length);
-		return (Obj.list_vedio);
-	}
-}).then((list_vedio) => {
-
-	console.log("load " + list_vedio.length + " videos");
-
-	// 开始解析网页
-	$(document).ready(function () {
-		console.log("ready to load");
-		//Obj = $("div.multirow-shelf").find("ul.shelf-content");
-		//尝试插入
-		//console.log("<ytd-grid-video-renderer class=\"style-scope ytd-grid-renderer\">"+
-		//  list_vedio[0].il +
-		//  "</ytd-grid-video-renderer>");
-		//$("<ytd-grid-video-renderer class=\"style-scope ytd-grid-renderer\">"+
-		//  list_vedio[0].il +
-		//  "</ytd-grid-video-renderer>").insertAfter($("div.style-scope.ytd-grid-renderer"));
-		//debug_1 = new infoVideo();
-		//debug_1 = getVideoInfo(list_vedio[0].il);
-		//debug_1.show();
-
-		//新版
-		//$("<ytd-grid-video-renderer class=\"style-scope ytd-grid-renderer\">"+
-		//  list_vedio[0].il +
-		//  "</ytd-grid-video-renderer>").insertAfter(
-		//								  $("ytd-grid-renderer.style-scope.ytd-shelf-renderer")
-		//								  .find("div.style-scope.ytd-grid-renderer")
-		//								  .find("ytd-grid-video-renderer.style-scope.ytd-grid-renderer"));
-
-		//旧版
-		// 首先复制一个视频作为模板
-		Obj = $("div.multirow-shelf").find("ul.shelf-content").find("li.yt-shelf-grid-item");
-
-		//console.log("<li class=\"yt-shelf-grid-item\">"+list_vedio[0].il +"</li>");
-		$(Obj).each(function (index) {
-			if (index == 0) {
-				ObjInsertModel = $(this).clone();
-				//console.log(ObjInsertModel);
-			}
-
-		});
-
-
-
-		$(Obj).each(function (index) {
-			if (index == 1) {
-				$(convertSearchToFeed(ObjInsertModel, list_vedio[0])).insertAfter($(this));
-			}
-
-		});
-
-
-
-	});
-});
+//console.log("load video list");
+////let  getListVedio = browser.storage.local.get("list_vedio");
+////getListVedio.then(onGot);
+//let gettingItem = browser.storage.local.get("list_vedio");
+//gettingItem.then((Obj) => {
+//	if (isObjEmpty(Obj)) {
+//		//console.log("list is empty");
+//
+//		return [];
+//	} else {
+//		//console.log("list is not empty");
+//		//console.log(Obj);
+//		//console.log(Obj.list_vedio.length);
+//		return (Obj.list_vedio);
+//	}
+//}).then((list_vedio) => {
+//
+//	console.log("load " + list_vedio.length + " videos");
+//
+//	// 开始解析网页
+//	$(document).ready(function () {
+//		console.log("ready to load");
+//		//Obj = $("div.multirow-shelf").find("ul.shelf-content");
+//		//尝试插入
+//		//console.log("<ytd-grid-video-renderer class=\"style-scope ytd-grid-renderer\">"+
+//		//  list_vedio[0].il +
+//		//  "</ytd-grid-video-renderer>");
+//		//$("<ytd-grid-video-renderer class=\"style-scope ytd-grid-renderer\">"+
+//		//  list_vedio[0].il +
+//		//  "</ytd-grid-video-renderer>").insertAfter($("div.style-scope.ytd-grid-renderer"));
+//		//debug_1 = new infoVideo();
+//		//debug_1 = getVideoInfo(list_vedio[0].il);
+//		//debug_1.show();
+//
+//		//新版
+//		//$("<ytd-grid-video-renderer class=\"style-scope ytd-grid-renderer\">"+
+//		//  list_vedio[0].il +
+//		//  "</ytd-grid-video-renderer>").insertAfter(
+//		//								  $("ytd-grid-renderer.style-scope.ytd-shelf-renderer")
+//		//								  .find("div.style-scope.ytd-grid-renderer")
+//		//								  .find("ytd-grid-video-renderer.style-scope.ytd-grid-renderer"));
+//
+//		//旧版
+//		// 首先复制一个视频作为模板
+//		Obj = $("div.multirow-shelf").find("ul.shelf-content").find("li.yt-shelf-grid-item");
+//
+//		//console.log("<li class=\"yt-shelf-grid-item\">"+list_vedio[0].il +"</li>");
+//		$(Obj).each(function (index) {
+//			if (index == 0) {
+//				ObjInsertModel = $(this).clone();
+//				//console.log(ObjInsertModel);
+//			}
+//
+//		});
+//
+//
+//
+//		$(Obj).each(function (index) {
+//			if (index == 1) {
+//				$(convertSearchToFeed(ObjInsertModel, list_vedio[0])).insertAfter($(this));
+//			}
+//
+//		});
+//
+//
+//
+//	});
+//});
 
 //接收到消息插入信息
+
+//$(document).ready(function () {
+$(window).ready(function () {
 browser.runtime.onMessage.addListener((request) => {
 	console.log(request.greeting)
 	console.log("load video list");
@@ -139,7 +142,7 @@ browser.runtime.onMessage.addListener((request) => {
 		}
 	}).then((list_vedio) => {
 		console.log("load " + list_vedio.length + " videos");
-		$(document).ready(function () {
+		//$(document).ready(function () {
 			console.log("ready to load");
 			Obj = $("div.multirow-shelf").find("ul.shelf-content").find("li.yt-shelf-grid-item");
 			$(Obj).each(function (index) {
@@ -152,7 +155,9 @@ browser.runtime.onMessage.addListener((request) => {
 					$(convertSearchToFeed(ObjInsertModel, list_vedio[0])).insertAfter($(this));
 				}
 			});
-		});
+		//});
 	});
 	return Promise.resolve({ response: "Hi, from content script" })
 })
+});
+
