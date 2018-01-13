069c934d5b4313e376fbab9e93462f5d98f84afa
bsplu <ruc.bs.plu@hotmail.com>
2018-01-13 22:34:07 +0100

    添加对新版本Youtube支持(未完成)
    插入视频后，youtube似乎会检查一遍插入的视频，然后将视频抹除。

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,160 +1,176 @@
 //================================FUNCTIONS====================================
-function convertSearchToFeed(ObjM, VedioInfo) {
+function convertSearchToFeed(ObjM, VedioInfo, vYoutube = 0) {
 	// 替换观看历史
 	// 替换标题
 	// 替换up主
 	ObjM_local = ObjM.clone();
 
-	// 首先替换封面
-	$(ObjM_local).find("span.yt-thumb-simple").find("img").attr('src', VedioInfo.coverUrl);
-	//console.log($(ObjList).find("img").attr("width"));
-	// 替换封面链接
-	
-	//$(ObjM_local).find("div.yt-lockup-thumbnail.contains-percent-duration-watched.contains-addto").find("a.yt-uix-sessionlink.spf-link").remove();
-	//console.log("change cover");
-	//$(ObjM_local).find("div.yt-lockup-thumbnail.contains-percent-duration-watched.contains-addto").prepend(
-	//	function () {
-	//		console.log("change cover2 ");
-	//		if($(VedioInfo.il).find("div.yt-lockup-thumbnail").find("a.yt-uix-sessionlink").length > 0){
-	//			console.log("existed");
-	//			return $(VedioInfo.il).find("div.yt-lockup-thumbnail").find("a.yt-uix-sessionlink").attr("classs", " yt-uix-sessionlink      spf-link ");
-	//		}else{
-	//			console.log("no existed");
-	//			return $(VedioInfo.il).find("div.yt-lockup-thumbnail.yt-pl-thumb").find("a.yt-pl-thumb-link.yt-uix-sessionlink.spf-link")
-	//			.attr("classs", " yt-uix-sessionlink      spf-link ");
-	//		}
-	//	}
-	//)
-	//$(ObjM_local).find("div.yt-lockup-thumbnail.contains-addto").find("a.yt-uix-sessionlink.spf-link")
-	//.attr("href",  $(VedioInfo.il).find("div.yt-lockup-thumbnail.yt-pl-thumb")
-	//.find("a.yt-pl-thumb-link.yt-uix-sessionlink.spf-link").attr("href"));
-	$(ObjM_local).find("div.yt-lockup-thumbnail.contains-addto").find("a.yt-uix-sessionlink.spf-link")
-	.attr("href",  $(VedioInfo.il).find("h3.yt-lockup-title").find("a").attr("href"));	
-	//console.log($(VedioInfo.il).find("h3.yt-lockup-title").find("a").attr("href")
-	//);
-	//console.log("change cover");
-	//$(ObjM_local).find("div.yt-lockup-thumbnail.contains-addto").prepend(
-	//	function () {
-	//		console.log("change cover2 ");
-	//		//if(VedioInfo){
-	//			//console.log("list existed");
-	//			ObjList = $(VedioInfo.il).find("div.yt-lockup-thumbnail.yt-pl-thumb")
-	//			.find("a.yt-pl-thumb-link.yt-uix-sessionlink.spf-link").attr("class", " yt-uix-sessionlink      spf-link ").clone();
-	//			//删除列表
-	//			$(ObjList).find("div.sidebar").remove();
-	//			// 更改图片尺寸
-	//			//$(ObjList).find("img").attr("width", "196");
-	//			//$(ObjList).find("img").attr("height", "110");
-	//			return $(ObjList);
-	//		//}else{
-	//			//console.log("no existed");
-	//			//return $(VedioInfo.il).find("div.yt-lockup-thumbnail.yt-pl-thumb").find("a.yt-pl-thumb-link.yt-uix-sessionlink.spf-link")
-	//			//.attr("classs", " yt-uix-sessionlink      spf-link ");
-	//		//}
-	//	}
-	//)
-	
-	
-	// 替换时长
-	$(ObjM_local).find("span.video-time").text(VedioInfo.videoTime);
+	if(vYoutube == 0){
 
-	// 替换标题
-	//删除原标题
-	$(ObjM_local).find("h3.yt-lockup-title.contains-action-menu").children().remove();
-	// 添加新标题,时长
-	$(ObjM_local).find("h3.yt-lockup-title.contains-action-menu").prepend($(VedioInfo.il).find("h3.yt-lockup-title").children());
+		// 首先替换封面
+		$(ObjM_local).find("span.yt-thumb-simple").find("img").attr('src', VedioInfo.coverUrl);
+		//console.log($(ObjList).find("img").attr("width"));
+		// 替换封面链接
 
-	// 替换up
-	$(ObjM_local).find("div.yt-lockup-byline.yt-ui-ellipsis.yt-ui-ellipsis-2").children().remove();
-	$(ObjM_local).find("div.yt-lockup-byline.yt-ui-ellipsis.yt-ui-ellipsis-2").prepend($(VedioInfo.il).find("div.yt-lockup-byline").children());
+		
+		$(ObjM_local).find("div.yt-lockup-thumbnail.contains-addto").find("a.yt-uix-sessionlink.spf-link")
+		.attr("href",  $(VedioInfo.il).find("h3.yt-lockup-title").find("a").attr("href"));	
+		
+		// 替换时长
+		$(ObjM_local).find("span.video-time").text(VedioInfo.videoTime);
 
-	// 替换观看次数 更新时间
-	$(ObjM_local).find("ul.yt-lockup-meta-info").children().remove();
-	$(VedioInfo.il).find("ul.yt-lockup-meta-info").children().each(function (index) {
-		$(ObjM_local).find("ul.yt-lockup-meta-info").prepend($(this));
-	});
+		// 替换标题
+		//删除原标题
+		$(ObjM_local).find("h3.yt-lockup-title.contains-action-menu").children().remove();
+		// 添加新标题,时长
+		$(ObjM_local).find("h3.yt-lockup-title.contains-action-menu").prepend($(VedioInfo.il).find("h3.yt-lockup-title").children());
+
+		// 替换up
+		$(ObjM_local).find("div.yt-lockup-byline.yt-ui-ellipsis.yt-ui-ellipsis-2").children().remove();
+		$(ObjM_local).find("div.yt-lockup-byline.yt-ui-ellipsis.yt-ui-ellipsis-2").prepend($(VedioInfo.il).find("div.yt-lockup-byline").children());
+
+		// 替换观看次数 更新时间
+		$(ObjM_local).find("ul.yt-lockup-meta-info").children().remove();
+		$(VedioInfo.il).find("ul.yt-lockup-meta-info").children().each(function (index) {
+			$(ObjM_local).find("ul.yt-lockup-meta-info").prepend($(this));
+		});
 
 
-	// 检查视频是否观看过
-	$(ObjM_local).find("span.resume-playback-background").remove();
-	$(ObjM_local).find("span.resume-playback-progress-bar").remove();
-	Objresume = $(VedioInfo.il).find("span.resume-playback-progress-bar");
-	if ($(Objresume).length > 0) {
-		$(ObjM_local).find("div.yt-lockup-thumbnail.contains-percent-duration-watched.contains-addto").append($(VedioInfo.il).find("span.resume-playback-background"));
-		$(ObjM_local).find("div.yt-lockup-thumbnail.contains-percent-duration-watched.contains-addto").append($(Objresume));
+		// 检查视频是否观看过
+		$(ObjM_local).find("span.resume-playback-background").remove();
+		$(ObjM_local).find("span.resume-playback-progress-bar").remove();
+		Objresume = $(VedioInfo.il).find("span.resume-playback-progress-bar");
+		if ($(Objresume).length > 0) {
+			$(ObjM_local).find("div.yt-lockup-thumbnail.contains-percent-duration-watched.contains-addto").append($(VedioInfo.il).find("span.resume-playback-background"));
+			$(ObjM_local).find("div.yt-lockup-thumbnail.contains-percent-duration-watched.contains-addto").append($(Objresume));
 
+		}
+	}else if(vYoutube == 1){
+
+		// console.log("1");
+		// 首先替换封面
+		$(ObjM_local).find("img#img").attr('src', VedioInfo.coverUrl);
+		//console.log($(ObjList).find("img").attr("width"));
+		// console.log("2");
+		// 替换封面链接
+		// console.log($(ObjM_local).find("a#thumbnail").attr("href"));
+		// console.log($(VedioInfo.il).find("h3.yt-lockup-title").find("a").attr("href"));
+		$(ObjM_local).find("a#thumbnail")
+		.attr("href",  $(VedioInfo.il).find("h3.yt-lockup-title").find("a").attr("href"));	
+
+		// console.log("3");
+		// 替换时长
+		$(ObjM_local).find("span.style-scope.ytd-thumbnail-overlay-time-status-renderer").text(VedioInfo.videoTime);
+		// console.log("4");
+		// 替换标题
+		$(ObjM_local).find("h3.style-scope.ytd-grid-video-renderer").find("a#video-title")
+		.attr('href', $(VedioInfo.il).find("h3.yt-lockup-title").find("a").attr("href")); 
+
+		$(ObjM_local).find("h3.style-scope.ytd-grid-video-renderer").find("a#video-title")
+		.attr('aria-label', "YTSE");  // 抹掉之前的信息
+
+		$(ObjM_local).find("h3.style-scope.ytd-grid-video-renderer").find("a#video-title")
+		.attr('title', $(VedioInfo.il).find("h3.yt-lockup-title").find("a").attr("title")); 
+
+		$(ObjM_local).find("h3.style-scope.ytd-grid-video-renderer").find("a#video-title")
+		.text($(VedioInfo.il).find("h3.yt-lockup-title").find("a").text()); 
+		// console.log("5");
+		// 替换up
+		$(ObjM_local).find("yt-formatted-string#byline").find("a")
+		.attr('href', $(VedioInfo.il).find("div.yt-lockup-byline").find("a.yt-uix-sessionlink").attr('href'));
+
+		$(ObjM_local).find("yt-formatted-string#byline").find("a")
+		.text($(VedioInfo.il).find("div.yt-lockup-byline").find("a.yt-uix-sessionlink").text());
+
+		// console.log("6");
+		// 替换观看次数 更新时间
+
+		$($(ObjM_local).find("div#metadata-line").children()[0])
+		.text($($(VedioInfo.il).find("ul.yt-lockup-meta-info").children()[0]).text());
+		$($(ObjM_local).find("div#metadata-line").children()[1])
+		.text($($(VedioInfo.il).find("ul.yt-lockup-meta-info").children()[1]).text());
+
+
+
+		// 检查视频是否观看过
+		// $(ObjM_local).find("span.resume-playback-background").remove();
+		// $(ObjM_local).find("span.resume-playback-progress-bar").remove();
+		// Objresume = $(VedioInfo.il).find("span.resume-playback-progress-bar");
+		// if ($(Objresume).length > 0) {
+		// 	$(ObjM_local).find("div.yt-lockup-thumbnail.contains-percent-duration-watched.contains-addto").append($(VedioInfo.il).find("span.resume-playback-background"));
+		// 	$(ObjM_local).find("div.yt-lockup-thumbnail.contains-percent-duration-watched.contains-addto").append($(Objresume));
+
+		// }
+	}else{
+		// 未知版本
 	}
 
 	return ObjM_local;
 }
 
-function getFeedVideoInfo(il_video) {
+function getFeedVideoInfo(il_video,vYoutube=0) {
     /*\ 
-    || 
-    \*/
-    if ($(il_video).find("ul.yt-lockup-meta.yt-lockup-playlist-items").length > 0) {
-        // 在对channel页面进行搜索的时候无法屏蔽列表,所以这里过滤一下是否为列表
-        //该条目是列表
-        vInfo = new infoVideo(il_video);
-        return vInfo;
-    } else {
-        // 不是列表
-//        titleObj = $(il_video).find("a.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2.yt-uix-sessionlink.spf-link");
-//        var title = $(titleObj).text();
-//        var videoUrl = $(titleObj).attr("href");
-//
-//        //获取时长,和封面
-//        //coverObj = $(il_video).find("a.yt-uix-sessionlink.spf-link").find("div.yt-thumb.video-thumb").find("span.yt-thumb-simple");
-//		//if(coverObj === undefined){
-//		//	var coverUrl_onload = $(coverObj).find("img").attr("data-thumb");
-//		//}else{
-//		//	var coverUrl_onload = $(coverObj).find("img").attr("data-thumb");
-//		//	if (coverUrl_onload === undefined) {
-//		//		var coverUrl = $(coverObj).find("img").attr("src");
-//		//	} else {
-//		//		var coverUrl = coverUrl_onload;
-//		//	}			
-//		//}
-//		var coverUrl_onload = $(il_video).find("img").attr("data-thumb");
-//		if (coverUrl_onload === undefined) {
-//			var coverUrl = $(il_video).find("img").attr("src");
-//		}else{
-//			var coverUrl = coverUrl_onload;
-//		}
-		
-        //var videoTime = $(il_video).find("span.video-time").text();
-        ////console.log(videoTime);
-        //// 获取频道信息
-        //channelObj = $(il_video).find("div.yt-lockup-content").find("div.yt-lockup-byline").find("a.yt-uix-sessionlink.spf-link");
-        //var channelName = $(channelObj).text();
-        //var channelUrl = $(channelObj).attr("href");
+    || vYoutube youtube页面版本
+	\*/
+	if(vYoutube == 0){
+		if ($(il_video).find("ul.yt-lockup-meta.yt-lockup-playlist-items").length > 0) {
+			// 在对channel页面进行搜索的时候无法屏蔽列表,所以这里过滤一下是否为列表
+			//该条目是列表
+			vInfo = new infoVideo(il_video);
+			return vInfo;
+		} else {
+			// 不是列表
 
-        // 获取更新时间
-        timeObj = $(il_video).find("div.yt-lockup-content").find("div.yt-lockup-meta").find("ul.yt-lockup-meta-info");
-        var tNow = new Date();
-        if ($(timeObj).find("li").toArray().length == 2) {
-            uptimeli = $(timeObj).find("li").toArray()[1];
-            uptimeStr = convertReTime2Int($(uptimeli).text()) + tNow.valueOf();
-        } else {
-            // 可能在直播
-            if ($(il_video).find("div.yt-lockup-content").find("div.yt-lockup-badges").find("ul.yt-badge-list").find("span.yt-badge.yt-badge-live").length > 0) {
-                // 在直播
-                uptimeStr = tNow.valueOf();
-            } else {
-                // 不知道类别
-                uptimeStr = tNow.valueOf();
-            }
-        }
+			// 获取更新时间
+			timeObj = $(il_video).find("div.yt-lockup-content").find("div.yt-lockup-meta").find("ul.yt-lockup-meta-info");
+			var tNow = new Date();
+			if ($(timeObj).find("li").toArray().length == 2) {
+				uptimeli = $(timeObj).find("li").toArray()[1];
+				uptimeStr = convertReTime2Int($(uptimeli).text()) + tNow.valueOf();
+			} else {
+				// 可能在直播
+				if ($(il_video).find("div.yt-lockup-content").find("div.yt-lockup-badges").find("ul.yt-badge-list").find("span.yt-badge.yt-badge-live").length > 0) {
+					// 在直播
+					uptimeStr = tNow.valueOf();
+				} else {
+					// 不知道类别
+					uptimeStr = tNow.valueOf();
+				}
+			}
 
 
 
-        //vInfo = new infoVideo($(il_video).html(), title, videoUrl, coverUrl, videoTime, channelName, channelUrl, uptimeStr, tNow);
+			//vInfo = new infoVideo($(il_video).html(), title, videoUrl, coverUrl, videoTime, channelName, channelUrl, uptimeStr, tNow);
+			vInfo = new infoVideo("", "", "", "", "", "", "", uptimeStr, tNow);
+
+			//vInfo.show();
+			return vInfo;
+
+		}
+	}else if(vYoutube == 1){ // 新版本
+		// 获取更新时间
+
+		var tNow = new Date();
+		// console.log($(il_video).find("div#metadata-line").children.length);
+		if($(il_video).find("div#metadata-line").children.length == 2){
+			// 不是直播
+			timeObj = $(il_video).find("div#metadata-line").children()[1];
+			//console.log(timeObj);
+			uptimeStr = convertReTime2Int($(timeObj).text()) + tNow.valueOf();
+		}else{
+			// 是直播
+			
+			uptimeStr = tNow.valueOf();
+		}
 		vInfo = new infoVideo("", "", "", "", "", "", "", uptimeStr, tNow);
 
-        //vInfo.show();
-        return vInfo;
-
-    }
+		// vInfo.show();
+		return vInfo;		
+	}else{
+		// 没有版本号对应
+		vInfo = new infoVideo(il_video);
+		return vInfo;		
+	}
 
     // vInfo.show();
 
@@ -170,6 +186,7 @@ function getFeedVideoInfo(il_video) {
 
 // 读取已获得的列表
 console.log("load video list");
+
 //let  getListVedio = browser.storage.local.get("list_vedio");
 //getListVedio.then(onGot);
 function Insert(){
@@ -178,7 +195,10 @@ function Insert(){
 		console.log("have inserted");
 	}else{
 		console.log("insert now");
+		let vYoutube = 0; //区分youtube版本， 旧版为0，新版为1. 考虑以后有更多版本，为整数
 		let gettingItem = browser.storage.local.get("list_vedio");
+		// let array_insertObj = new Array();
+		// let indexArray = new Array();
 		gettingItem.then((Obj) => {
 			if (isObjEmpty(Obj)) {
 				//console.log("list is empty");
@@ -219,16 +239,30 @@ function Insert(){
 		
 				//旧版
 				// 首先复制一个视频作为模板
-				Obj = $("div.multirow-shelf").find("ul.shelf-content").find("li.yt-shelf-grid-item");
-		
-				//console.log("<li class=\"yt-shelf-grid-item\">"+list_vedio[0].il +"</li>");
-				$(Obj).each(function (index) {
-					if (index == 0) {
-						ObjInsertModel = $(this).clone();
-						//console.log(ObjInsertModel);
+				Obj = $("div.multirow-shelf").find("ul.shelf-content").find("li.yt-shelf-grid-item"); // 旧版
+				
+				if(Obj.length == 0){
+					// 可能是新版
+					Obj = $("div#items").children(); //("ytd-item-section-renderer.style-scope.ytd-section-list-renderer"); 
+					//console.log(Obj);
+					vYoutube = 1;
+					if(Obj.length == 0){
+						// 既不是新版也不是旧版
+						return ;
 					}
+				}
+				console.log("Youtube version : ", vYoutube);
+				//console.log("<li class=\"yt-shelf-grid-item\">"+list_vedio[0].il +"</li>");
+
+				ObjInsertModel= $($(Obj).toArray()[0]).clone();
+
+				// $(Obj).each(function (index) {
+				// 	if (index == 0) {
+				// 		ObjInsertModel = $(this).clone();
+				// 		//console.log(ObjInsertModel);
+				// 	}
 		
-				});
+				// });
 				
 				
 				// 准备插入
@@ -236,18 +270,24 @@ function Insert(){
 				for (let i = 0; i < list_vedio.length; i++) {
 					let haveLoaded = false;
 					$(Obj).each(function (index) {
-						if (index >= indexBegin && !haveLoaded) {
-							
+						if (index >= indexBegin && !haveLoaded ) {
+							// console.log(i,index);
 							//$(convertSearchToFeed(ObjInsertModel, list_vedio[13])).insertAfter($(this));
 							// 输出订阅列表时间
 							//let vInfo = new infoVideo();
-							vInfo = getFeedVideoInfo(this); // 没有title和videoUrl
+							vInfo = getFeedVideoInfo(this, vYoutube); // 没有title和videoUrl
 							if(vInfo.upTime < list_vedio[i].upTime){
-								ObjIn = $(convertSearchToFeed(ObjInsertModel, list_vedio[i]));
+								console.log(i,index);
+
+								ObjIn = $(convertSearchToFeed(ObjInsertModel, list_vedio[i], vYoutube));
+								// console.log($(ObjIn).html());
+								$(ObjIn).attr("insert","insert");
 								$(ObjIn).css("border", "1px dashed #4CAF50"); //outline: #4CAF50 solid 10px;
 								//$(ObjIn).css("outline", "#4CAF50 solid 5px"); 
 								$(ObjIn).insertBefore($(this));
-								
+								// console.log($(ObjIn).find("a#thumbnail").attr("href"));
+								// array_insertObj.push($(ObjIn).html());
+								// indexArray.push(index);
 								// 添加框线
 								//$(this).next().css("border", "1px solid red");
 								indexBegin = indexBegin + 1;
@@ -272,13 +312,14 @@ function Insert(){
 				//	}
 				//
 				//});		
-		
-		
 			});
 		});
 	}
 }
 
+Insert(); //新版可以直接加载
+
+// 旧版需要使用监听...
 browser.runtime.onMessage.addListener(request => {
   //console.log("Message from the background script:");
   //console.log(request.greeting);
